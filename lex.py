7729cf259b908c8fca5622f2eba822f010519b3c
LorenaAquino <73086025+Lorena228@users.noreply.github.com>
2022-08-17 16:24:52 -0300

    Update lex.py

--- lex.py
+++ lex.py
@@ -1,8 +1,9 @@
 def lexer():
+    #Essa parte é pra informar o caminho do arquivo independente onde esteja
     caminho1 = input('Onde está o seu arquivo test.txt?')
     parte = caminho1.replace('\\', '\\\\')
     arquivo = input('Qual o nome do seu arquivo? (ex.:test.txt)')
     caminho2 = caminho1 + "\\" + arquivo
-    print("Caminho:", caminho2)
+    #Aqui o arquivo é chamado
     with open(caminho2) as teste:
         ""
